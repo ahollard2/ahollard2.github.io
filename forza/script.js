e667fd2e01df0688953fb7fb380dfe30b1f312b3
anthoshka(da_flex_is_here) <77466619+ahollard2@users.noreply.github.com>
2021-11-22 07:14:31 +0100

    Update script.js

--- forza/script.js
+++ forza/script.js
@@ -1,12 +1,8 @@
 // alert("Le JavaScript fonctionne !")
-let score;
-score = 0
+
 function maFonction() {
 	document.querySelector("#monPara").style.color="red";
-	
-
 }
-
 function foncRouge() {
 	document.querySelector("#monPara").classList.remove("vert");
 	document.querySelector("#monPara").classList.add("rouge");
@@ -15,18 +11,14 @@ function foncVert() {
 	document.querySelector("#monPara").classList.remove("rouge");
 	document.querySelector("#monPara").classList.add("vert");
 }
-
-
 function f_cat(){
 	alert("yo")
 	document.querySelector("#main_car").style.left=20;
 }
 
-
 function modifMessage() {
 	document.querySelector("#monPara").innerHTML = "Bravo, vous avez cliqué sur le bouton !"
 }
-
 function foncEntre(){
 	document.querySelector("#maDiv").classList.remove("blanc");
 	document.querySelector("#maDiv").classList.add("rouge");
@@ -35,50 +27,68 @@ function foncQuitte() {
 	document.querySelector("#maDiv").classList.remove("rouge");
 	document.querySelector("#maDiv").classList.add("blanc");
 }
-// #casey_x, 
-// let id_main_car;
-// let x,y;
-// x,y= 3,3
-// id_main_car = "#case"+x+"_"+y
-// document.querySelector("#case"+x+"_"+y).classList.add("main_car");
+function play(){
+	// document.querySelector("#explication").style.color=red;
+	document.getElementById("#menu").style.visibility=collapse;
+}
+
+let score = 0;
 let y = 7;
 let x = 6;
 let broken = false;
+let main_car = "main_car_1";//main_car represente la claase de la voiure utilisee
+
+//pour le menu
+document.querySelector("#menu_car1").classList.add("main_car_1");
+function car1(){
+	
+	document.querySelector("#menu_car2").classList.remove("selected_car");
+	main_car = "main_car_1";
+	document.querySelector("#menu_car1").classList.add("selected_car");
+	console.log(main_car);
+}
+function car2(){
+	document.querySelector("#menu_car1").classList.remove("selected_car");
+	main_car = "main_car_2";
+	document.querySelector("#menu_car2").classList.add("selected_car");
+	console.log(main_car);
+}
 
 document.querySelector("#case1_7").classList.add("car");
-// const yellow = document.getElementById("case"+x+""+y);
-// document.getElementById("case"+x+""+y).style.background="red";	
+
+	
 console.log(y,x);
-document.querySelector("#case"+y+"_"+x).classList.add("main_car");
+document.querySelector("#case"+y+"_"+x).classList.add(main_car);
 window.addEventListener("keypress",(e) =>{
 	
     console.log(e);
     console.log(y,x);
 
     if (e.key === "q" && x>5){
-        document.getElementById("case"+y+"_"+x).classList.remove("main_car");
+        document.getElementById("case"+y+"_"+x).classList.remove(main_car);
         x-=1;
 	}
     if (e.key === "d" && x<8){	
-        document.getElementById("case"+y+"_"+x).classList.remove("main_car");
+        document.getElementById("case"+y+"_"+x).classList.remove(main_car);
         x+=1;
 		}
     if (e.key === "z" && y>0){
-        document.getElementById("case"+y+"_"+x).classList.remove("main_car");
+        document.getElementById("case"+y+"_"+x).classList.remove(main_car);
     	y-=1;
 	}
     if (e.key === "s" && y<7){
-        document.getElementById("case"+y+"_"+x).classList.remove("main_car");
+        document.getElementById("case"+y+"_"+x).classList.remove(main_car);
 		y+=1;
 	}
 	if (x=== 7 && y ===1){
-		document.getElementById("case"+y+"_"+x).classList.add("main_car_broken");
+		document.getElementById("case"+y+"_"+x).classList.add(main_car+"broken");
 		broken = true;
 	}
 	else{
-		document.getElementById("case"+y+"_"+x).classList.add("main_car");
+		document.getElementById("case"+y+"_"+x).classList.add(main_car);
 	}	
 })
+
 
 // var rightPressed = false;
 // var leftPressed = false;
